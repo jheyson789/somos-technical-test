f695e68cfe18b6f198bb406550ce67d45d188dab
Jheyson Zurita Soria <jheysoria20@gmail.com>
2024-03-04 17:50:50 -0400

    header component

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
 import { PagesComponent } from './pages.component';
 import { RouterModule } from '@angular/router';
+import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
   declarations: [PagesComponent],
-  imports: [CommonModule, PagesRoutingModule, RouterModule],
+  imports: [CommonModule, PagesRoutingModule, RouterModule, ComponentsModule],
 })
 export class PagesModule {}
